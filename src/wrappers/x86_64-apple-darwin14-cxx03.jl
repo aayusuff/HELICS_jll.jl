f24a92403ed565ce90aa700903f21c1fe919700f
jlbuild <juliabuildbot@gmail.com>
2020-08-21 13:41:22 +0000

    HELICS_jll build 2.6.0+0

--- src/wrappers/x86_64-apple-darwin14-cxx03.jl
+++ src/wrappers/x86_64-apple-darwin14-cxx03.jl
@@ -9,7 +9,7 @@ LIBPATH_env = "DYLD_FALLBACK_LIBRARY_PATH"
 LIBPATH_default = "~/lib:/usr/local/lib:/lib:/usr/lib"
 
 # Relative path to `libhelicsSharedLib`
-const libhelicsSharedLib_splitpath = ["lib", "libhelicsSharedLib.2.5.2.dylib"]
+const libhelicsSharedLib_splitpath = ["lib", "libhelicsSharedLib.2.6.0.dylib"]
 
 # This will be filled out by __init__() for all products, as it must be done at runtime
 libhelicsSharedLib_path = ""
@@ -30,8 +30,6 @@ function __init__()
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
     # then append them to our own.
     foreach(p -> append!(PATH_list, p), (ZeroMQ_jll.PATH_list,))
@@ -48,12 +46,8 @@ function __init__()
     filter!(!isempty, unique!(PATH_list))
     filter!(!isempty, unique!(LIBPATH_list))
     global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
+    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
 
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    
 end  # __init__()
 
