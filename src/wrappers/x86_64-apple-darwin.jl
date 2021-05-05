ccfb4b13fa474c0393ee995163044eea470c09c1
jlbuild <juliabuildbot@gmail.com>
2021-05-05 17:23:20 +0000

    HELICS_jll build 2.7.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(ZeroMQ_jll)
     JLLWrappers.@init_library_product(
         libhelicsSharedLib,
-        "lib/libhelicsSharedLib.2.6.1.dylib",
+        "lib/libhelicsSharedLib.2.7.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
